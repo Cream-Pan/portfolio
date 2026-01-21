c06be2029a3b280d420cb7b19f2fe6eaea3f901d
Cream-Pan <aaotkt.05561519@gmail.com>
2026-01-22 00:14:06 +0900

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Takato Ishii | Portfolio",
   description: "Takato Ishii's Portfolio",
+  icons: {
+    icon: "/icon.png", 
+  },
 };
 
 export default function RootLayout({
